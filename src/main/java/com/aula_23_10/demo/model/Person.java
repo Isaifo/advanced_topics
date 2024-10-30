9c6e59ead8adfe8faa37e764e34ff1ed9c0d2b5c
Isaifo <ysoca98@hotmail.com>
2024-10-30 14:32:10 -0300

    feat: add paramets in person model

--- src/main/java/com/aula_23_10/demo/model/Person.java
+++ src/main/java/com/aula_23_10/demo/model/Person.java
@@ -1,5 +1,6 @@
 package com.aula_23_10.demo.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,8 +23,14 @@ public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column (name = "name", length = 100, nullable = false)
     private String name;
+
+    @Column (name = "meail", length = 100, nullable = false)
     private String email;
+
+    @Column (length = 255, nullable = false)
     private String password;
 
 
